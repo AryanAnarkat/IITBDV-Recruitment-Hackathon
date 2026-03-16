401d62533f22b27ab124a23dac1be82302aa90f5
AryanAnarkat <aryananarkat2007@gmail.com>
2026-03-16 12:38:13 +0530

    Update planner.py

--- PPC/racing_hkt_windows/dist/participant/planner.py
+++ PPC/racing_hkt_windows/dist/participant/planner.py
@@ -33,11 +33,20 @@ def plan(cones: list[dict]) -> list[dict]:
     # TODO: implement your path planning here
     blue = np.array([[cone["x"], cone["y"]] for cone in cones if cone["side"] == "left"])
     yellow = np.array([[cone["x"], cone["y"]] for cone in cones if cone["side"] == "right"])
-
+    a=0
     # implement a planning algorithm to generate a path from the blue and yellow cones
     for b, y in zip(blue, yellow):
+<<<<<<< HEAD
+        a=a+1
+=======
+>>>>>>> e3e03186d94c200776238a17de8022323b994332
         x=.48
         path.append({
         "x": (x*b[0] + y[0]) / (x+1),
         "y": (x*b[1] + y[1]) / (x+1)})
+<<<<<<< HEAD
+
+        
+=======
+>>>>>>> e3e03186d94c200776238a17de8022323b994332
     return path
