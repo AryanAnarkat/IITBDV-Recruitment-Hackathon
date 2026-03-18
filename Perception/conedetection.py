c87d1a33a1487b6b732af53d0b2b6e6bbc6141e0
Anarkat Manisha <AryanAnarkat>
2026-03-18 14:33:55 +0530

    percept

--- Perception/conedetection.py
+++ Perception/conedetection.py
@@ -1,10 +1,5 @@
 from ultralytics import YOLO
 import cv2
-import os
-print(os.getcwd())
-print(os.listdir())
-model = YOLO("yolov8n.pt")
-print("YOLO working!")
 model=YOLO("best.pt")
 image=cv2.imread("image.png")
 image2=image.copy()
@@ -18,7 +13,7 @@ for box in results[0].boxes:
     y2=int(box.xyxy[0][3])
     l=y2-y1
     cones.append(l)
-    cv2.rectangle(image2, (x1, y1), (x2, y2), (0,255,0), 2)
+    cv2.rectangle(image2, (x1, y1), (x2, y2), (108,108,0), 2)
     cv2.putText(image2, "d:"+str(int(30000/l)), (x1, y1-10),
             cv2.FONT_HERSHEY_COMPLEX,
             0.6, (0,255,0), 2)
